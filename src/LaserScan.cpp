50f966007e13314cade6f852e79507296ba1a999
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-06-14 18:31:27 +0200

    reg LaserScan

---
+++ src/LaserScan.cpp
@@ -0,0 +1,8 @@
+#include "RegisterItems.hpp"
+
+#include <asn1/LaserScan.h>
+//ENVIRE_REGISTER_ITEM(asn1SccLaserScan)
+ENVIRE_REGISTER_MULTI_ITEM_WITHOUT_SERIALIZATION(asn1SccLaserScan,10)
+
+
+//static envire::core::MetadataInitializer _metadataInitLaserScan(typeid(envire::core::Item<asn1SccLaserScan>), "asn1SccLaserScan", "10");
---
+++ src/RegisterItems.hpp
@@ -0,0 +1,23 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+
+#include <envire_core/plugin/Plugin.hpp>
+//#include <envire_core/items/Item.hpp>
+
+
+
+
+#define ENVIRE_REGISTER_MULTI_METADATA( _classname, _datatype, _variable ) \
+static envire::core::MetadataInitializer _metadataInit##_variable(typeid(_classname), #_datatype, #_classname);
+#define ENVIRE_REGISTER_MULTI_ITEM_WITHOUT_SERIALIZATION( _datatype, _variable ) \
+CLASS_LOADER_REGISTER_CLASS(envire::core::Item<_datatype>, envire::core::ItemBase) \
+ENVIRE_REGISTER_MULTI_METADATA(envire::core::Item<_datatype>, _datatype, _variable)
+
+//#include <asn1/LaserScan.h>
+//ENVIRE_REGISTER_ITEM(asn1SccLaserScan)
+//ENVIRE_REGISTER_MULTI_ITEM_WITHOUT_SERIALIZATION(asn1SccLaserScan,1)
+
+
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
