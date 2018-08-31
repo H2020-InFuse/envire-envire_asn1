707bc1d24754aef1cd112808005830a0bcdf9f49
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-08-31 11:27:47 +0200

    added CMakeLists.txt reminder to gen script

--- genterateTypeRegistrationFromTemplate.sh
+++ genterateTypeRegistrationFromTemplate.sh
@@ -4,3 +4,6 @@ TYPENAME=$1
 
 cat src/Register.cpp.template | sed -e "s/TYPENAME/${TYPENAME}/g" > src/Register${TYPENAME}.cpp
 git add src/Register${TYPENAME}.cpp
+
+
+echo "to finish, please add Register${TYPENAME}.cpp to the SOURCED in the srs/CMakeLists.txt"
