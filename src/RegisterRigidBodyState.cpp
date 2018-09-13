63f27cd2124099b6fa0d87bb0b9654f6a7d3c6e7
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-09-13 17:22:58 +0200

    fixed rbs serialization

--- src/RegisterRigidBodyState.cpp
+++ src/RegisterRigidBodyState.cpp
@@ -7,21 +7,47 @@
 BOOST_SERIALIZATION_SPLIT_FREE(asn1SccRigidBodyState)
 template<class Archive> inline void save(Archive & ar, const asn1SccRigidBodyState & value, const unsigned int file_version){
     
-    byte data[asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING];
-    
     int pErrCode;
-    BitStream asnbitstream = envire_asn::initBitStream(data,asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING);
+    byte data[asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING];
+    BitStream asnbitstream;//     = envire_asn::initBitStream(data,asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING);
+    BitStream_Init(&asnbitstream,data,asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING);
+
     asn1SccRigidBodyState_Encode(&value, &asnbitstream, &pErrCode, false);
 
+    // for (int i =0;i<3;++i){
+    //     printf("%.2f ",value.pos.arr[i]);
+    // }
+
+    // asn1SccRigidBodyState teststate;
+    // teststate.pos.arr[0] = 0;
+    // teststate.pos.arr[1] = 0;
+    // teststate.pos.arr[2] = 0;
+    // byte data2[asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING];
+    // BitStream asnbitstream2 ;//= envire_asn::initBitStream(data2,asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING);
+    // BitStream_Init(&asnbitstream2,data2,asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING);
+
+    // memcpy(data2,data,asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING);
+    // asn1SccRigidBodyState_Decode(&teststate, &asnbitstream2, &pErrCode);
+
+
+
+    // for (int i =0;i<3;++i){
+    //     printf("%.2f ",teststate.pos.arr[i]);
+    // }
+    
+    // printf("\n");
+
     ar << boost::serialization::make_binary_object(data, asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING);
+
 }
 template<class Archive> inline void load(Archive & ar, asn1SccRigidBodyState & value, const unsigned int file_version){
-    
-    byte data[asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING];
-    ar >> boost::serialization::make_binary_object(data, asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING);
 
     int pErrCode;
-    BitStream asnbitstream = envire_asn::initBitStream(data,asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING);
+    byte data[asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING];
+    BitStream asnbitstream;// = envire_asn::initBitStream(data,asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING);
+    BitStream_Init(&asnbitstream,data,asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING);
+
+    ar >> boost::serialization::make_binary_object(data, asn1SccRigidBodyState_REQUIRED_BYTES_FOR_ENCODING);
     asn1SccRigidBodyState_Decode(&value, &asnbitstream, &pErrCode);
 }
 ENVIRE_REGISTER_ITEM(asn1SccRigidBodyState)
