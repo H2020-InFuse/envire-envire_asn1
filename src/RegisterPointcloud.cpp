85ff601525b3f82aacae73c19d6cb8cba4193aae
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2018-10-18 14:36:24 +0200

    use dynamic allocated buffer for pointcloud serialization

--- src/RegisterPointcloud.cpp
+++ src/RegisterPointcloud.cpp
@@ -9,21 +9,25 @@ BOOST_SERIALIZATION_SPLIT_FREE(asn1SccPointcloud)
 template<class Archive> inline void save(Archive & ar, const asn1SccPointcloud & value, const unsigned int file_version){
     
     int pErrCode;
-    byte data[asn1SccPointcloud_REQUIRED_BYTES_FOR_ENCODING];
+    std::vector<byte> buffer;
+    buffer.resize(asn1SccPointcloud_REQUIRED_BYTES_FOR_ENCODING);
+    
     BitStream asnbitstream;
-    BitStream_Init(&asnbitstream,data,asn1SccPointcloud_REQUIRED_BYTES_FOR_ENCODING);
+    BitStream_Init(&asnbitstream,buffer.data(),asn1SccPointcloud_REQUIRED_BYTES_FOR_ENCODING);
 
     asn1SccPointcloud_Encode(&value, &asnbitstream, &pErrCode, false);
-    ar << boost::serialization::make_binary_object(data, asn1SccPointcloud_REQUIRED_BYTES_FOR_ENCODING);
+    ar << boost::serialization::make_binary_object(buffer.data(), asn1SccPointcloud_REQUIRED_BYTES_FOR_ENCODING);
 }
 template<class Archive> inline void load(Archive & ar, asn1SccPointcloud & value, const unsigned int file_version){
     
     int pErrCode;
-    byte data[asn1SccPointcloud_REQUIRED_BYTES_FOR_ENCODING];
-    BitStream asnbitstream;
-    BitStream_Init(&asnbitstream,data,asn1SccPointcloud_REQUIRED_BYTES_FOR_ENCODING);
+    std::vector<byte> buffer;
+    buffer.resize(asn1SccPointcloud_REQUIRED_BYTES_FOR_ENCODING);
 
-    ar >> boost::serialization::make_binary_object(data, asn1SccPointcloud_REQUIRED_BYTES_FOR_ENCODING);
+    BitStream asnbitstream;
+    BitStream_Init(&asnbitstream,buffer.data(),asn1SccPointcloud_REQUIRED_BYTES_FOR_ENCODING);
+
+    ar >> boost::serialization::make_binary_object(buffer.data(), asn1SccPointcloud_REQUIRED_BYTES_FOR_ENCODING);
     asn1SccPointcloud_Decode(&value, &asnbitstream, &pErrCode);
 }
 ENVIRE_REGISTER_ITEM(asn1SccPointcloud)
